6e8757275f5b4527b2983d867a490ed5be9453ea
Julio Chrisostomo <julio.chrisostomo@gmail.com>
2018-04-04 16:58:32 -0300

    changed ruby installation

--- dev_install.sh
+++ dev_install.sh
@@ -34,3 +34,21 @@ sdk install gradle
 npm install ngrok -g
 npm install http-server -g
 npm install yarn -g
+
+#Ruby installation using rbenv to avoid gem install permisions error
+sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
+
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+exec $SHELL
+
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
+exec $SHELL
+
+rbenv install 2.3.1
+rbenv global 2.3.1
+
+sudo gem update --system
+sudo gem install compass
