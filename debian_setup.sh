51d791875249e42542cf2611be816718699b21a8
Julio Chrisostomo <julio.chrisostomo@gmail.com>
2021-10-07 22:12:55 -0300

    Update debian_setup.sh

--- debian_setup.sh
+++ debian_setup.sh
@@ -1,84 +1,84 @@
 #!/bin/bash
 #### update system
-apt update && apt upgrade -y
+sudo apt update && sudo apt upgrade -y
 
 #### Utilities
-apt install snapd -y
-snap install core
+sudo apt install snapd -y
+sudo snap install core
 
-apt install zip -y
-apt install curl -y
-apt install materia-gtk-theme -y
-apt install papirus-icon-theme -y
-apt install gnome-tweak-tool -y
-apt install piper -y
-apt install psensor -y
-apt install gparted -y
-apt install stacer -y
-apt install net-tools -y
-apt install jq -y
-apt install speedtest-cli -y
-apt install ffmpeg -y
-apt install vlc -y
-apt install gnome-sushi -y
-apt install neofetch -y
-apt install python3-pip -y
-apt install digikam -y
-apt install handbrake -y
-apt install drawing -y
-snap install spotify
+sudo apt install zip -y
+sudo apt install curl -y
+sudo apt install materia-gtk-theme -y
+sudo apt install papirus-icon-theme -y
+sudo apt install gnome-tweak-tool -y
+sudo apt install piper -y
+sudo apt install psensor -y
+sudo apt install gparted -y
+sudo apt install stacer -y
+sudo apt install net-tools -y
+sudo apt install jq -y
+sudo apt install speedtest-cli -y
+sudo apt install ffmpeg -y
+sudo apt install vlc -y
+sudo apt install gnome-sushi -y
+sudo apt install neofetch -y
+sudo apt install python3-pip -y
+sudo apt install digikam -y
+sudo apt install handbrake -y
+sudo apt install drawing -y
+sudo snap install spotify
 
 #### Dev
-apt install git -y
-apt install tilix -y
-apt install openfortivpn -y
-apt install openjdk-17-jdk -y
-apt install mysql-server -y
-apt install meld -y
-apt install htop -y
-apt install ctop -y
-apt install vim -y
-apt install maven -y
-apt install filezilla -y
-snap install slack
-snap install ngrok
-snap install gitkraken --classic
-snap install postman
-snap install slack --classic
-snap install code --classic
-snap install intellij-idea-community --classic
-snap install datagrip --classic
-snap install android-studio-canary --classic
-snap install beekeeper-studio
-apt install qemu-kvm -y
-usermod -aG kvm $USER
-apt install virt-manager -y
+sudo apt install git -y
+sudo apt install tilix -y
+sudo apt install openfortivpn -y
+sudo apt install openjdk-17-jdk -y
+sudo apt install mysql-server -y
+sudo apt install meld -y
+sudo apt install htop -y
+sudo apt install ctop -y
+sudo apt install vim -y
+sudo apt install maven -y
+sudo apt install filezilla -y
+sudo snap install slack
+sudo snap install ngrok
+sudo snap install gitkraken --classic
+sudo snap install postman
+sudo snap install slack --classic
+sudo snap install code --classic
+sudo snap install intellij-idea-community --classic
+sudo snap install datagrip --classic
+sudo snap install android-studio-canary --classic
+sudo snap install beekeeper-studio
+sudo apt install qemu-kvm -y
+sudo usermod -aG kvm $USER
+sudo apt install virt-manager -y
 
 #### purge unused apps
-apt purge aisleriot -y
-apt purge five-or-more -y
-apt purge four-in-a-row -y
-apt purge hitori -y
-apt purge lightsoff -y
-apt purge quadrapassel -y
-apt purge reversi -y
-apt purge shotwell -y
-apt purge totem -y
-apt purge evolution -y
-apt purge gnome-games -y
-apt purge gnome-clocks -y
-apt purge gnome-contacts -y
-apt purge gnome-klotski -y
-apt purge gnome-maps -y
-apt purge gnome-sound-recorder -y
-apt purge gnome-todo -y
-apt purge gnome-music -y
-apt purge gnome-weather -y
-apt purge libreoffice-draw -y
-apt purge libreoffice-impress -y
+sudo apt purge aisleriot -y
+sudo apt purge five-or-more -y
+sudo apt purge four-in-a-row -y
+sudo apt purge hitori -y
+sudo apt purge lightsoff -y
+sudo apt purge quadrapassel -y
+sudo apt purge reversi -y
+sudo apt purge shotwell -y
+sudo apt purge totem -y
+sudo apt purge evolution -y
+sudo apt purge gnome-games -y
+sudo apt purge gnome-clocks -y
+sudo apt purge gnome-contacts -y
+sudo apt purge gnome-klotski -y
+sudo apt purge gnome-maps -y
+sudo apt purge gnome-sound-recorder -y
+sudo apt purge gnome-todo -y
+sudo apt purge gnome-music -y
+sudo apt purge gnome-weather -y
+sudo apt purge libreoffice-draw -y
+sudo apt purge libreoffice-impress -y
 
 #### clean installation
-apt autopurge
+sudo apt autopurge
 
 echo '---------------------------------------------------------------------'
 echo '| install sdkman via https://sdkman.io/install                      |'
