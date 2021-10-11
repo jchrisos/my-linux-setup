ed21d36f9de81225614e251d5b47f323b1a5926b
Julio Chrisostomo <julio.chrisostomo@gmail.com>
2021-10-11 01:38:20 -0300

    Update debian_setup.sh

--- debian_setup.sh
+++ debian_setup.sh
@@ -59,11 +59,11 @@ sudo apt install virt-manager -y
 
 #### install docker
 sudo apt remove docker docker-engine docker.io containerd runc -y
-sudo apt install  -y
 sudo apt apt-transport-https  -y
 sudo apt ca-certificates  -y
 sudo apt gnupg -y
 sudo apt lsb-release -y
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
 sudo apt install docker-ce docker-ce-cli containerd.io -y
